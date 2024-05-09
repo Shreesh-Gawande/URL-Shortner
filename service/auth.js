01e48b94eb3418f8e62dbc2a1bb349a2df618fb1
Shreesh-Gawande <shreeshgawande10@gmail.com>
2024-05-09 20:22:24 +0530

    Completed stateless authentication

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,21 @@
-const sessionIdToUserMap=new Map();  //This is basically the diary in which server keep track of all the ids
+ const jwt= require("jsonwebtoken");
+ const secret="Shreesh10#";
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+function setUser(user){
+    return jwt.sign({
+        _id:user._id,
+        email:user.email,
+    },secret);
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+    if(!token) return null;
+    try {
+        return jwt.verify(token,secret);
+    } catch (error) {
+        return null;
+    }
+    
 }
 
 
